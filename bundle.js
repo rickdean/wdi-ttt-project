6f03544abbe284101b1b8c2f6c3e242040f807d7
rickdean <rickdean@outlook.com>
2016-11-03 16:38:52 -0400

    Commit to GH Pages with updates to localhost
    Commit changes to localhost reference

--- bundle.js
+++ bundle.js
@@ -290,8 +290,8 @@ webpackJsonp([0],[
 	'use strict';
 
 	var config = {
-	  host: 'http://localhost:4741'
-
+	  //host:'http://localhost:4741',
+	  host: 'https://aqueous-atoll-85096.herokuapp.com'
 	};
 
 	module.exports = config;
