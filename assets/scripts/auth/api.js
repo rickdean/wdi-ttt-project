7f4145ac0b85dc92f57b657e1e2e96636cd903cf
rickdean <rickdean@outlook.com>
2016-11-14 12:38:40 -0500

    COMMIT Updates to DATA COMMIT updates to Data

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const config = require('../config.js');
 const store = require('../store.js');
 const app = require('./app.js');
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -68,7 +68,6 @@ const addHandlers = () => {
 
 };
 
-
 module.exports = {
   addHandlers,
 };
